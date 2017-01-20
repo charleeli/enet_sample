0e632464d0e1d05fe1139c846f6c0e5570e11068
charleeli <717066513@qq.com>
2017-01-20 21:03:02 +0800

    bug fix

---
+++ example/complex/client_enet.lua
@@ -0,0 +1,60 @@
+package.cpath = "./build/luaclib/?.so;./build/luaclib/levent/?.so"
+package.path = package.path..";./lualib/?.lua"..";./example/simple/?.lua"..";./build/lualib/?.lua"
+
+local pretty = require 'pl.pretty'
+local enet = require "enet"
+local log = require "log"
+log.init{log_basename = "simple_client",service_name = "simple server" }
+
+local SprotoLoader = require "sprotoloader"
+local SprotoEnv = require "sproto_env"
+SprotoEnv.init('./build/sproto')
+
+local s2c_sp = SprotoLoader.load(SprotoEnv.PID_S2C)
+local s2c_host = s2c_sp:host(SprotoEnv.BASE_PACKAGE)
+local c2s_client = s2c_host:attach(SprotoLoader.load(SprotoEnv.PID_C2S))
+
+local host = enet.host_create()
+local server = host:connect("localhost:5678")
+
+local session = 0
+
+local function send_request(name, args)
+    session = session + 1
+    local v = c2s_client(name, args, session)
+    server:send(v)
+end
+
+local count = 0
+while count < 100 do
+	local event = host:service(100)
+	if event then
+		if event.type == "receive" then
+			log.info("Got message: %s",  event.data)
+
+			local type, session, response = s2c_host:dispatch(event.data)
+
+			print('----',type, session)
+			pretty.dump(response)
+			print('----')
+		else
+			log.info("Got event %s", event.type)
+		end
+	end
+
+	if count == 8 then
+		log.info("sending message")
+
+		local ok, err = pcall(send_request, "send_private_chat", {uuid=123,msg="hello"})
+		if not ok then
+			print('send err', cmd, args, err)
+		end
+	end
+
+	count = count + 1
+end
+
+server:disconnect()
+host:flush()
+
+log.info("done")
--- test/test_thread.lua
+++ test/test_thread.lua
@@ -4,30 +4,25 @@ package.path = package.path..";./lualib/?.lua"..";./example/simple/?.lua"
 
 local uv = require('luv')
 
-local timer_id = uv.new_thread(function()
-    local uv = require('luv')
-
-    local function set_interval(interval, callback)
-        local timer = uv.new_timer()
-        local function ontimeout()
-            callback(timer)
-        end
-        uv.timer_start(timer, interval, interval, ontimeout)
-        return timer
+local function set_interval(interval, callback)
+    local timer = uv.new_timer()
+    local function ontimeout()
+        callback(timer)
     end
+    uv.timer_start(timer, interval, interval, ontimeout)
+    return timer
+end
 
-    set_interval(3000, function()
-        print("interval...")
-    end)
-
-    uv.run()
+set_interval(3000, function()
+    print("interval...")
 end)
+
 
 local hare_id = uv.new_thread(function()
     local uv = require('luv')
     while true do
         uv.sleep(1000)
-        print("Hare ran ++++++")
+        print("Hare ran +++++++")
     end
 end)
 
@@ -35,10 +30,16 @@ local tortoise_id = uv.new_thread(function()
     local uv = require('luv')
     while true do
         uv.sleep(2000)
-        print("Tortoise ran ******")
+        print("Tortoise ran ********")
     end
 end)
 
-uv.thread_join(timer_id)
+
+while true do
+    print('-------')
+    uv.sleep(10)
+end
+
+uv.run()
 uv.thread_join(hare_id)
 uv.thread_join(tortoise_id)
